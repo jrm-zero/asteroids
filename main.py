0d3a8495178911aee0f5080be5879c159b04b908
zerosugar <git@jrsort.com>
2026-02-11 21:21:42 -0600

    collision added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from constants import *
 import pygame
 from logger import log_state
+from logger import log_event
 from player import *
+from asteroidfield import *
+import sys
 
 def main():
     print(f"Starting Asteroids with pygame version: {str(pygame.version.ver)}")
@@ -10,17 +13,26 @@ def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player) == True:
+                log_event("player hit")
+                print("Game over!")
+                sys.exit()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
